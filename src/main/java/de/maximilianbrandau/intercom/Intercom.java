c2a2d73439245256b5448a5ffd7de082422dc65a
Maximilian Brandau <mbrandau@online.de>
2018-07-08 11:16:27 +0200

    Add javadoc

--- src/main/java/de/maximilianbrandau/intercom/Intercom.java
+++ src/main/java/de/maximilianbrandau/intercom/Intercom.java
@@ -5,10 +5,22 @@ import de.maximilianbrandau.intercom.server.IntercomServer;
 
 public class Intercom {
 
+    /**
+     * Convenience method to create an {@link IntercomClient.Builder}.
+     *
+     * @param host The hostname of your intercom server
+     * @param port The port the intercom server is listening on
+     * @return An intercom client builder
+     */
     public static IntercomClient.Builder client(String host, int port) {
         return new IntercomClient.Builder(host, port);
     }
 
+    /**
+     * Convenience method to create an {@link IntercomServer.Builder}.
+     * @param port The port that the server should listen on
+     * @return An intercom server builder
+     */
     public static IntercomServer.Builder server(int port) {
         return new IntercomServer.Builder(port);
     }
